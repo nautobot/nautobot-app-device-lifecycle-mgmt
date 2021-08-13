eaa2842a5f20fbd83d84dccb7f7ac7176a63b9f9
Josh Silvas <josh@jsilvas.com>
2021-08-13 11:00:40 -0500

    Removes devices relationship in the graphql hardware type.

--- nautobot_device_lifecycle_mgmt/graphql/types.py
+++ nautobot_device_lifecycle_mgmt/graphql/types.py
@@ -7,27 +7,8 @@ from nautobot_device_lifecycle_mgmt.models import HardwareLCM
 from nautobot_device_lifecycle_mgmt.filters import HardwareLCMFilterSet
 
 
-class LCMDevice(DjangoObjectType):
-    """GraphQL representation of a Nautobot Device."""
-
-    class Meta:
-        """Metadata magic method for the DjangoObjectType."""
-
-        model = Device
-
-
 class HardwareLCMType(DjangoObjectType):
     """Graphql Type Object for the Device Lifecycle model."""
-
-    devices = graphene.List(LCMDevice)
-
-    def resolve_devices(self, info, **kwargs):
-        """Custom resolver to filter devices that are part of the hardware LifeCycle object."""
-        if self.device_type:
-            return Device.objects.filter(device_type=self.device_type)
-        if self.inventory_item:
-            return Device.objects.filter(inventoryitems__part_id=self.inventory_item)
-        return []
 
     class Meta:
         """Metadata magic method for the HardwareLCMType."""
