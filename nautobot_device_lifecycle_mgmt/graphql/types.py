1f1ff007a27ac85521ff36ad9b3f7debb962659b
Josh Silvas <josh@jsilvas.com>
2021-08-13 11:03:39 -0500

    Update imports after removing virtual device.

--- nautobot_device_lifecycle_mgmt/graphql/types.py
+++ nautobot_device_lifecycle_mgmt/graphql/types.py
@@ -1,8 +1,5 @@
 """GraphQL implementation for the Device LifeCycle Management plugin."""
-import graphene
-
 from nautobot.extras.graphql.types import DjangoObjectType
-from nautobot.dcim.models import Device
 from nautobot_device_lifecycle_mgmt.models import HardwareLCM
 from nautobot_device_lifecycle_mgmt.filters import HardwareLCMFilterSet
 
