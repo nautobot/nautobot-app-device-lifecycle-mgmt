52886544e023a4134b1f54a223650c9d9d7cfa2a
Przemek Rogala <progala@progala.net>
2021-09-24 21:19:53 +0100

    Remove duplicate url paths introduced in 848b2d0d4b081bc431876950d6aab9332ba302fa

--- nautobot_device_lifecycle_mgmt/urls.py
+++ nautobot_device_lifecycle_mgmt/urls.py
@@ -106,36 +106,4 @@ urlpatterns = [
         kwargs={"model": ContactLCM},
     ),
     path("contact/import/", views.ContactLCMBulkImportView.as_view(), name="contactlcm_import"),
-    # Software LifeCycle Management URLs
-    path("software/", views.SoftwareLCMListView.as_view(), name="softwarelcm_list"),
-    path("software/<uuid:pk>/", views.SoftwareLCMView.as_view(), name="softwarelcm"),
-    path("software/add/", views.SoftwareLCMCreateView.as_view(), name="softwarelcm_add"),
-    path("software/<uuid:pk>/delete/", views.SoftwareLCMDeleteView.as_view(), name="softwarelcm_delete"),
-    path("software/<uuid:pk>/edit/", views.SoftwareLCMEditView.as_view(), name="softwarelcm_edit"),
-    path(
-        "software/<uuid:pk>/changelog/",
-        ObjectChangeLogView.as_view(),
-        name="softwarelcm_changelog",
-        kwargs={"model": SoftwareLCM},
-    ),
-    # ValidatedSoftware
-    path("validated-software/", views.ValidatedSoftwareLCMListView.as_view(), name="validatedsoftwarelcm_list"),
-    path("validated-software/<uuid:pk>/", views.ValidatedSoftwareLCMView.as_view(), name="validatedsoftwarelcm"),
-    path("validated-software/add/", views.ValidatedSoftwareLCMEditView.as_view(), name="validatedsoftwarelcm_add"),
-    path(
-        "validated-software/<uuid:pk>/delete/",
-        views.ValidatedSoftwareLCMDeleteView.as_view(),
-        name="validatedsoftwarelcm_delete",
-    ),
-    path(
-        "validated-software/<uuid:pk>/edit/",
-        views.ValidatedSoftwareLCMEditView.as_view(),
-        name="validatedsoftwarelcm_edit",
-    ),
-    path(
-        "validated-software/<uuid:pk>/changelog/",
-        ObjectChangeLogView.as_view(),
-        name="validatedsoftwarelcm_changelog",
-        kwargs={"model": ValidatedSoftwareLCM},
-    ),
 ]
