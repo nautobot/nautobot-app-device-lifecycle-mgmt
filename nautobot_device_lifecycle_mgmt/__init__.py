c80e4aef41080dbb8d16715eadb8038f0f7a7d98
Przemek Rogala <progala@progala.net>
2021-11-24 15:43:12 +0000

    Use DjangoObject from Graphene lib. Remove GQL types that already use decorator.

--- nautobot_device_lifecycle_mgmt/__init__.py
+++ nautobot_device_lifecycle_mgmt/__init__.py
@@ -30,14 +30,25 @@ class DeviceLifeCycleConfig(PluginConfig):
 
     def ready(self):
         """Register custom signals."""
-        super().ready()
+        import graphene  # pylint: disable=import-outside-toplevel
+        from graphene_django.converter import convert_django_field  # pylint: disable=import-outside-toplevel
+        from taggit.managers import TaggableManager  # pylint: disable=import-outside-toplevel
+        from nautobot.extras.graphql.types import TagType  # pylint: disable=import-outside-toplevel
+
         import nautobot_device_lifecycle_mgmt.signals  # pylint: disable=C0415,W0611 # noqa: F401
+
+        @convert_django_field.register(TaggableManager)
+        def convert_field_to_list_tags(field, registry=None):
+            """Convert TaggableManager to List of Tags."""
+            return graphene.List(TagType)
 
         from .signals import (  # pylint: disable=import-outside-toplevel
             post_migrate_create_relationships,
         )
 
         post_migrate.connect(post_migrate_create_relationships, sender=self)
+
+        super().ready()
 
 
 config = DeviceLifeCycleConfig  # pylint:disable=invalid-name
