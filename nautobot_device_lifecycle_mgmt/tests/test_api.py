9af47d231f9fcb99ead40bd498e7f2f1c959ccf5
Przemek Rogala <progala@progala.net>
2022-02-11 11:06:58 +0000

    Add API tests

--- nautobot_device_lifecycle_mgmt/tests/test_api.py
+++ nautobot_device_lifecycle_mgmt/tests/test_api.py
@@ -5,7 +5,7 @@ from django.contrib.contenttypes.models import ContentType
 
 from nautobot.utilities.testing import APIViewTestCases
 from nautobot.dcim.models import DeviceType, Manufacturer, Platform, Device, DeviceRole, InventoryItem, Site
-from nautobot.extras.models import Status
+from nautobot.extras.models import Status, Tag
 
 from nautobot_device_lifecycle_mgmt.models import (
     HardwareLCM,
@@ -15,6 +15,7 @@ from nautobot_device_lifecycle_mgmt.models import (
     ValidatedSoftwareLCM,
     CVELCM,
     VulnerabilityLCM,
+    SoftwareImage,
 )
 from nautobot_device_lifecycle_mgmt.tests.conftest import create_devices, create_cves, create_softwares
 
@@ -501,3 +502,150 @@ class VulnerabilityLCMAPITest(
 
     def test_options_objects_returns_display_and_value(self):
         """Disabling inherited test that uses POST."""
+
+
+class SoftwareImageAPITest(APIViewTestCases.APIViewTestCase):  # pylint: disable=too-many-ancestors
+    """Test the SoftwareImage API."""
+
+    model = SoftwareImage
+    brief_fields = [
+        "default_image",
+        "device_types",
+        "display",
+        "download_url",
+        "id",
+        "image_file_checksum",
+        "image_file_name",
+        "inventory_items",
+        "object_tags",
+        "url",
+    ]
+
+    @classmethod
+    def setUpTestData(cls):
+        """Create a superuser and token for API calls."""
+        device_platform_cisco, _ = Platform.objects.get_or_create(name="Cisco IOS", slug="cisco_ios")
+        device_platform_arista, _ = Platform.objects.get_or_create(name="Arista EOS", slug="arista_eos")
+        softwares_cisco = (
+            SoftwareLCM.objects.get_or_create(
+                **{
+                    "device_platform": device_platform_cisco,
+                    "version": "17.3.3 MD",
+                }
+            )[0],
+            SoftwareLCM.objects.get_or_create(
+                **{
+                    "device_platform": device_platform_cisco,
+                    "version": "15.5(1)SY",
+                }
+            )[0],
+        )
+        softwares_arista = (
+            SoftwareLCM.objects.get_or_create(
+                **{
+                    "device_platform": device_platform_arista,
+                    "version": "4.21.6M",
+                }
+            )[0],
+            SoftwareLCM.objects.get_or_create(
+                **{
+                    "device_platform": device_platform_arista,
+                    "version": "4.25.7F",
+                }
+            )[0],
+        )
+
+        status_active = Status.objects.get(slug="active")
+        manufacturer_cisco = Manufacturer.objects.create(name="Cisco", slug="cisco")
+        manufacturer_arista = Manufacturer.objects.create(name="Arista", slug="arista")
+        site = Site.objects.create(name="Site 1", slug="site-1")
+        devicerole = DeviceRole.objects.create(name="Router", slug="router", color="ff0000")
+        devicetypes_cisco = (
+            DeviceType.objects.create(manufacturer=manufacturer_cisco, model="ASR-1000", slug="asr-1000"),
+            DeviceType.objects.create(manufacturer=manufacturer_cisco, model="Catalyst 6500", slug="catalyst-6500"),
+        )
+        devicetypes_arista = (
+            DeviceType.objects.create(manufacturer=manufacturer_arista, model="7150S", slug="7150s"),
+            DeviceType.objects.create(manufacturer=manufacturer_arista, model="7508R", slug="7508r"),
+        )
+        device_cisco = Device.objects.create(
+            device_type=devicetypes_cisco[0], device_role=devicerole, name="Device 1", site=site, status=status_active
+        )
+        device_arista = Device.objects.create(
+            device_type=devicetypes_arista[0], device_role=devicerole, name="Device 2", site=site, status=status_active
+        )
+        inventoryitems_cisco = (
+            InventoryItem.objects.create(device=device_cisco, name="SwitchModule1"),
+            InventoryItem.objects.create(device=device_cisco, name="Supervisor Engine 720"),
+        )
+        inventoryitems_arista = (
+            InventoryItem.objects.create(device=device_arista, name="SFP1"),
+            InventoryItem.objects.create(device=device_arista, name="QSFP1"),
+        )
+
+        tags = (Tag.objects.create(name="asr", slug="asr"), Tag.objects.create(name="edge", slug="edge"))
+
+        print(SoftwareImage.objects.all())
+
+        cls.create_data = [
+            {
+                "image_file_name": "asr1001x-universalk9.17.03.03.SPA.bin",
+                "software": softwares_cisco[0].id,
+                "device_types": [devicetype.pk for devicetype in devicetypes_cisco],
+                "download_url": "ftp://device-images.local.com/cisco/asr1001x-universalk9.17.03.03.SPA.bin",
+                "image_file_checksum": "9cf2e09b59207a4d8ea408ea9971f6ae6facab9a1855a34e1ed8755f3ffe4b9e14",
+                "default_image": True,
+            },
+            {
+                "image_file_name": "asr1001x-universalk9.17.03.03.ssl.SPA.bin",
+                "software": softwares_cisco[0].id,
+                "inventory_items": [inventoryitem.pk for inventoryitem in inventoryitems_cisco],
+                "download_url": "ftp://device-images.local.com/cisco/asr1001x-universalk9.17.03.03.ssl.SPA.bin",
+                "image_file_checksum": "7f2e09b59207a4d8ea40fa132va71f6ae6facab9a1851a34e1ed8755f3ffe4b9e14",
+                "default_image": False,
+            },
+            {
+                "image_file_name": "s2t54-ipservicesk9_npe-mz.SPA.155-1.SY1.bin",
+                "software": softwares_cisco[1].id,
+                "object_tags": [tag.pk for tag in tags],
+                "download_url": "ftp://device-images.local.com/cisco/s2t54-ipservicesk9_npe-mz.SPA.155-1.SY1.bin",
+                "image_file_checksum": "74e61320f5518a2954b2d307b7e6a038",
+                "default_image": True,
+            },
+        ]
+        software_image = SoftwareImage(
+            image_file_name="eos2gb_4.21.6m.swi",
+            software=softwares_arista[0],
+            download_url="ftp://images.local/arista/eos2gb_4.21m.swi",
+            image_file_checksum="78arfabd75b0fa2vzas1e7afcc6faa3fc",
+            default_image=True,
+        )
+        software_image.save()
+        software_image = SoftwareImage(
+            image_file_name="eos_4.21.6m.swi",
+            software=softwares_arista[0],
+            download_url="ftp://images.local/arista/eos_4.21.6m.swi",
+            image_file_checksum="78arfabd75b0fa2vzas1e7a7ac6faa3fc",
+            default_image=True,
+        )
+        software_image.inventory_items.set([inventoryitem.pk for inventoryitem in inventoryitems_arista])
+        software_image.save()
+
+        software_image = SoftwareImage(
+            image_file_name="eos_4.25.7f.swi",
+            software=softwares_arista[1],
+            download_url="ftp://images.local/arista/eos_4.25.7f.swi",
+            image_file_checksum="78arfabd75b0fa2vfas1e7a7ac6faa3fc",
+            default_image=True,
+        )
+        software_image.device_types.set([devicetype.pk for devicetype in devicetypes_arista])
+        software_image.save()
+
+    def test_bulk_create_objects(self):
+        """Currently don't support bulk operations."""
+
+    def test_bulk_delete_objects(self):
+        """Currently don't support bulk operations."""
+
+    def test_bulk_update_objects(self):
+        """Currently don't support bulk operations."""
