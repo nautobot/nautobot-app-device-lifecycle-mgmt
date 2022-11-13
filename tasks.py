bcfe7b53e316e6e84499b715b13520d3fb8d96d0
Brad Haas <bradh11@gmail.com>
2022-11-13 18:11:25 -0500

    fix tasks.py plugin reference for update

--- tasks.py
+++ tasks.py
@@ -90,7 +90,7 @@ def docker_compose(context, command, **kwargs):
         "NAUTOBOT_VER": context.nautobot_device_lifecycle_mgmt.nautobot_ver,
         "PYTHON_VER": context.nautobot_device_lifecycle_mgmt.python_ver,
     }
-    compose_command = f'docker-compose --project-name {context.nautobot_device_lifecycle_mgmt.project_name} --project-directory "{context.nautobot_firewall_models.compose_dir}"'
+    compose_command = f'docker-compose --project-name {context.nautobot_device_lifecycle_mgmt.project_name} --project-directory "{context.nautobot_device_lifecycle_mgmt.compose_dir}"'
     for compose_file in context.nautobot_device_lifecycle_mgmt.compose_files:
         compose_file_path = os.path.join(context.nautobot_device_lifecycle_mgmt.compose_dir, compose_file)
         compose_command += f' -f "{compose_file_path}"'
