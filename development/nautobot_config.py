d3ad8e1cffcc1ed3e2b738db7083a3171998f4c9
Bradford Haas <bradh11@gmail.com>
2022-11-16 13:25:23 -0500

    Update development/nautobot_config.py
    Co-authored-by: Przemek Rogala <progala@progala.net>

--- development/nautobot_config.py
+++ development/nautobot_config.py
@@ -264,7 +264,11 @@ PLUGINS = [
 # Plugins configuration settings. These settings are used by various plugins that the user may have installed.
 # Each key in the dictionary is the name of an installed plugin and its value is a dictionary of settings.
 PLUGINS_CONFIG = {
-    "nautobot_device_lifecycle_mgmt": {},
+    "nautobot_device_lifecycle_mgmt": {
+        "barchart_bar_width": float(os.environ.get("BARCHART_BAR_WIDTH", 0.1)),
+        "barchart_width": int(os.environ.get("BARCHART_WIDTH", 12)),
+        "barchart_height": int(os.environ.get("BARCHART_HEIGHT", 5)),
+    },
 }
 
 # When determining the primary IP address for a device, IPv6 is preferred over IPv4 by default. Set this to True to
