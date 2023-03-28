c5109529454d5fec23da68edd5d763a7fd4ab60d
pato23arg <patricior_villar@hotmail.com>
2023-03-28 15:37:16 +0200

    changes to metric.py

--- nautobot_device_lifecycle_mgmt/metrics.py
+++ nautobot_device_lifecycle_mgmt/metrics.py
@@ -18,6 +18,7 @@ def nautobot_metrics_dlcm_eos():
     current_dt = datetime.now()
     hw_eos_notices = HardwareLCM.objects.filter(end_of_support__lte=current_dt)
     hw_eos_device_types = [notice.device_type for notice in hw_eos_notices]
+    hw_eos_inventoryitems = [notice.inventory_item for notice in hw_eos_notices]
 
     part_number_gauge = GaugeMetricFamily(
         "nautobot_lcm_devices_eos_per_part_number", "Nautobot LCM Devices EOS per Part Number", labels=["part_number"]
@@ -28,7 +29,7 @@ def nautobot_metrics_dlcm_eos():
 
     for notice in hw_eos_notices:
         if notice.device_type:
-            part_number = notice.device_type.slug
+            part_number = notice.device_type.part_number
             eos_devices = Device.objects.filter(device_type=notice.device_type)
             metric_value = eos_devices.count()
         elif notice.inventory_item:
@@ -36,15 +37,17 @@ def nautobot_metrics_dlcm_eos():
             eos_devices = InventoryItem.objects.filter(part_id=notice.inventory_item)
             metric_value = eos_devices.count()
         else:
-            part_number = 'UNKNOWN'
-            metric_value = 0
+            continue
+
         part_number_gauge.add_metric(labels=[part_number], value=metric_value)
    
     yield part_number_gauge
 
     for site in Site.objects.all():
-        eos_devices_in_site = Device.objects.filter(site=site, device_type__in=hw_eos_device_types)
-        devices_gauge.add_metric(labels=[site.slug], value=eos_devices_in_site.count())
+        eos_devices_in_site = Device.objects.filter(site=site, device_type__in=hw_eos_device_types).count()
+        eos_inventoryitems_in_site = InventoryItem.objects.filter(part_id__in=hw_eos_inventoryitems, device__site=site.id).count()
+        metric_value = eos_devices_in_site + eos_inventoryitems_in_site
+        devices_gauge.add_metric(labels=[site.slug], value=metric_value)
     
     yield devices_gauge
 
