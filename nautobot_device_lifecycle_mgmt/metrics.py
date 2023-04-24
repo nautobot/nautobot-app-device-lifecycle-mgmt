d17a8689341fcec0782c98829cd2d0b62bd0a8c0
pvillar_netdev <pato23arg@users.noreply.github.com>
2023-04-24 20:06:36 +0200

    Update metrics.py

--- nautobot_device_lifecycle_mgmt/metrics.py
+++ nautobot_device_lifecycle_mgmt/metrics.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 
 from django.conf import settings
-from nautobot.dcim.models import Device, Site, InventoryItem
+from nautobot.dcim.models import Device, Site, InventoryItem, DeviceType
 from prometheus_client.core import GaugeMetricFamily
 
 from nautobot_device_lifecycle_mgmt.models import HardwareLCM
@@ -12,6 +12,7 @@ PLUGIN_SETTINGS = settings.PLUGINS_CONFIG.get("nautobot_device_lifecycle_mgmt", 
 
 def nautobot_metrics_dlcm_eos():
     """Calculate number of End-of-Support (EOS) devices per Device Type and per Site.
+
     Yields:
         GaugeMetricFamily: Prometheus Metrics
     """
@@ -26,18 +27,31 @@ def nautobot_metrics_dlcm_eos():
     devices_gauge = GaugeMetricFamily(
         "nautobot_lcm_devices_eos_per_site", "Nautobot LCM Devices EOS per Site", labels=["site"]
     )
-
+    device_type_filter, inventory_item_filter = [], []
     for notice in hw_eos_notices:
         if notice.device_type:
             part_number = notice.device_type.part_number if notice.device_type.part_number else notice.device_type.slug
-            eos_devices_count = Device.objects.filter(device_type=notice.device_type).count()
+            metric_value = Device.objects.filter(device_type=notice.device_type).count()
+            device_type_filter.append(notice.device_type.slug)
         elif notice.inventory_item:
             part_number = notice.inventory_item
-            eos_devices_count = InventoryItem.objects.filter(part_id=notice.inventory_item).count()
-        else:
-            continue
+            metric_value = InventoryItem.objects.filter(part_id=notice.inventory_item).count()
+            inventory_item_filter.append(notice.inventory_item)
 
-        part_number_gauge.add_metric(labels=[part_number], value=eos_devices_count)
+        part_number_gauge.add_metric(labels=[part_number], value=metric_value)
+
+    device_types = DeviceType.objects.all().exclude(slug__in=device_type_filter)
+    inventory_items = InventoryItem.objects.all().exclude(part_id__in=inventory_item_filter)
+
+    for device_type in device_types:
+        metric_value = 0
+        part_number = device_type.part_number if device_type.part_number else device_type.slug
+        part_number_gauge.add_metric(labels=[part_number], value=metric_value)
+
+    for inventory_item in inventory_items:
+        metric_value = 0
+        part_number = inventory_item.part_id if inventory_item.part_id else inventory_item.slug
+        part_number_gauge.add_metric(labels=[part_number], value=metric_value)
 
     yield part_number_gauge
 
