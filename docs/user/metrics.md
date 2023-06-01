d63b53320919c7e8e9a03cf3168b7f26892912ce
Allen Hogan <98360253+alhogan@users.noreply.github.com>
2023-06-01 04:44:36 -0500

    Prometheus Metrics (#148)
    * Add valid software by device_type gauge
    
    * Add metrics for inventory items and totals
    
    ---------
    
    Co-authored-by: Zack Tobar <zack.tobar@networktocode.com>
    Co-authored-by: Przemek Rogala <progala@progala.net>
    Co-authored-by: Bradford Haas <bradh11@gmail.com>

--- docs/user/metrics.md
+++ docs/user/metrics.md
@@ -3,9 +3,19 @@
 !!! warning
     Metrics will only work with Nautobot Version 1.5.13 and newer.
 
-The Software Lifecycle applicaiton has the following metrics included.
+The Device Lifecycle application has the following metrics included:
 
 ```
+# HELP nautobot_lcm_compliance_by_device_type_total Number of devices that have valid/invalid software by device_type
+# TYPE nautobot_lcm_compliance_by_device_type_total gauge
+nautobot_lcm_compliance_by_device_type_total{device_type="ASR-9903",is_valid="True"} 3.0
+nautobot_lcm_compliance_by_device_type_total{device_type="ASR-9903",is_valid="False"} 1.0
+nautobot_lcm_compliance_by_device_type_total{device_type="IntelServer",is_valid="True"} 2.0
+nautobot_lcm_compliance_by_device_type_total{device_type="IntelServer",is_valid="False"} 2.0
+# HELP nautobot_lcm_valid_by_inventory_item_total Number of devices that have valid/invalid software by inventory item
+# TYPE nautobot_lcm_valid_by_inventory_item_total gauge
+nautobot_lcm_valid_by_inventory_item_total{inventory_item="Cisco-FEX-port1",is_valid="True"} 0.0
+nautobot_lcm_valid_by_inventory_item_total{inventory_item="Cisco-FEX-port1",is_valid="False"} 1.0
 # HELP Nautobot LCM Hardware End of Support per Part Number
 # TYPE nautobot_lcm_hw_end_of_support_per_part_number gauge
 nautobot_lcm_devices_eos_per_part_number{part_number="WS-SUP720-3BXL"} 38.0
--- nautobot_device_lifecycle_mgmt/metrics.py
+++ nautobot_device_lifecycle_mgmt/metrics.py
@@ -1,12 +1,114 @@
 """Nautobot Device LCM plugin application level metrics ."""
 from datetime import datetime
 
-from django.db.models import Case, Count, F, IntegerField, Q, When, Value, OuterRef, Subquery
+from django.db.models import Case, Count, F, IntegerField, OuterRef, Q, Subquery, Value, When
 from django.db.models.functions import Coalesce
 from nautobot.dcim.models import Device, DeviceType, InventoryItem, Site
 from prometheus_client.core import GaugeMetricFamily
 
-from nautobot_device_lifecycle_mgmt.models import HardwareLCM
+from nautobot_device_lifecycle_mgmt.models import (
+    DeviceSoftwareValidationResult,
+    HardwareLCM,
+    InventoryItemSoftwareValidationResult,
+)
+
+
+def metrics_lcm_validation_report_device_type():
+    """Calculate number of devices with valid/invalid software by device_type.
+
+    Yields:
+        GaugeMetricFamily: Prometheus Metrics
+    """
+    device_software_compliance_gauge = GaugeMetricFamily(
+        "nautobot_lcm_software_compliance_per_device_type",
+        "Number of devices that have valid/invalid software by device_type",
+        labels=["device_type", "is_valid"],
+    )
+
+    device_types = DeviceType.objects.values("model")
+
+    # Annotate DeviceSoftwareValidationResult with counts for valid and invalid software per device type
+    validation_counts = (
+        DeviceSoftwareValidationResult.objects.order_by()
+        .values(model=F("device__device_type__model"))
+        .annotate(
+            invalid_count=Count("device__device_type__model", filter=Q(is_validated=False)),
+            valid_count=Count("device__device_type__model", filter=Q(is_validated=True)),
+        )
+        .values("model", "invalid_count", "valid_count")
+    )
+
+    # Set valid and invalid software validation counts to 0 for device types that don't have DeviceSoftwareValidationResult entry
+    validation_counts_final = device_types.annotate(
+        valid_count=Coalesce(
+            Subquery(validation_counts.filter(model=OuterRef("model")).values("valid_count")), Value(0)
+        ),
+        invalid_count=Coalesce(
+            Subquery(validation_counts.filter(model=OuterRef("model")).values("invalid_count")), Value(0)
+        ),
+    ).values_list("model", "valid_count", "invalid_count")
+
+    for model, valid_count, invalid_count in validation_counts_final:
+        device_software_compliance_gauge.add_metric(
+            labels=[model, "True"],
+            value=valid_count,
+        )
+        device_software_compliance_gauge.add_metric(
+            labels=[model, "False"],
+            value=invalid_count,
+        )
+
+    yield device_software_compliance_gauge
+
+
+def metrics_lcm_validation_report_inventory_item():
+    """Calculate number of inventory items with valid/invalid software.
+
+    InventoryItem objects missing part_id are excluded.
+
+    Yields:
+        GaugeMetricFamily: Prometheus Metrics
+    """
+    inventory_item_software_compliance_gauge = GaugeMetricFamily(
+        "nautobot_lcm_software_compliance_per_inventory_item",
+        "Number of devices that have valid/invalid software by inventory item",
+        labels=["inventory_item", "is_valid"],
+    )
+    inventory_item_part_ids = InventoryItem.objects.exclude(part_id="").order_by().values("part_id").distinct()
+
+    # Annotate InventoryItemSoftwareValidationResult with counts for valid and invalid software per part id
+    validation_counts = (
+        InventoryItemSoftwareValidationResult.objects.exclude(inventory_item__part_id="")
+        .order_by()
+        .values(part_id=F("inventory_item__part_id"))
+        .annotate(
+            invalid_count=Count("inventory_item__part_id", filter=Q(is_validated=False)),
+            valid_count=Count("inventory_item__part_id", filter=Q(is_validated=True)),
+        )
+        .values("part_id", "invalid_count", "valid_count")
+    )
+
+    # Set valid and invalid software validation counts to 0 for part ids that don't have InventoryItemSoftwareValidationResult entry
+    validation_counts_final = inventory_item_part_ids.annotate(
+        valid_count=Coalesce(
+            Subquery(validation_counts.filter(part_id=OuterRef("part_id")).values("valid_count")), Value(0)
+        ),
+        invalid_count=Coalesce(
+            Subquery(validation_counts.filter(part_id=OuterRef("part_id")).values("invalid_count")), Value(0)
+        ),
+    ).values_list("part_id", "valid_count", "invalid_count")
+
+    for part_id, valid_count, invalid_count in validation_counts_final:
+        inventory_item_software_compliance_gauge.add_metric(
+            labels=[part_id, "True"],
+            value=valid_count,
+        )
+        inventory_item_software_compliance_gauge.add_metric(
+            labels=[part_id, "False"],
+            value=invalid_count,
+        )
+
+    yield inventory_item_software_compliance_gauge
 
 
 def metrics_lcm_hw_end_of_support():  # pylint: disable=too-many-locals
@@ -120,4 +222,6 @@ def metrics_lcm_hw_end_of_support():  # pylint: disable=too-many-locals
 
 metrics = [
     metrics_lcm_hw_end_of_support,
+    metrics_lcm_validation_report_device_type,
+    metrics_lcm_validation_report_inventory_item,
 ]
