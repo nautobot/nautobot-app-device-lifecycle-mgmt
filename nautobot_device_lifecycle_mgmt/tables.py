9ac9e46feb8392d4ca884a0ad1f69f9d695ee547
Adam Byczkowski <38091261+qduk@users.noreply.github.com>
2024-01-09 14:12:14 -0600

    Updated per review

--- nautobot_device_lifecycle_mgmt/tables.py
+++ nautobot_device_lifecycle_mgmt/tables.py
@@ -443,8 +443,6 @@ class ContractLCMTable(BaseTable):
         template_code="""{% load humanize %}{{ record.cost|intcomma }}{% if record.currency %} {{ record.currency }}{% endif %}"""
     )
     actions = ButtonsColumn(ContractLCM, buttons=("changelog", "edit", "delete"))
-    start = tables.DateColumn(format="m-d-Y")
-    end = tables.DateColumn(format="m-d-Y")
 
     class Meta(BaseTable.Meta):  # pylint: disable=too-few-public-methods
         """Meta attributes."""
