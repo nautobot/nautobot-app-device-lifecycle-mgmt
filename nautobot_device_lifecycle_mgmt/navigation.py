84cdadf6c0b3557cf808f84891581216735f14a6
David Cates <david.cates@networktocode.com>
2024-07-31 14:09:38 -0600

    update navigation permissions

--- nautobot_device_lifecycle_mgmt/navigation.py
+++ nautobot_device_lifecycle_mgmt/navigation.py
@@ -173,14 +173,14 @@ menu_items = (
                         link="plugins:nautobot_device_lifecycle_mgmt:hardwarenotice_device_report",
                         name="Device Hardware Notice - Report",
                         permissions=[
-                            "nautobot_device_lifecycle_mgmt.view_hardwarelcm",
+                            "nautobot_device_lifecycle_mgmt.view_devicehardwarenoticeresult",
                         ],
                     ),
                     NavMenuItem(
                         link="plugins:nautobot_device_lifecycle_mgmt:devicehardwarenoticeresult_list",
                         name="Device Hardware Notice - List",
                         permissions=[
-                            "nautobot_device_lifecycle_mgmt.view_hardwarelcm",
+                            "nautobot_device_lifecycle_mgmt.view_devicehardwarenoticeresult",
                         ],
                     ),
                     NavMenuItem(
