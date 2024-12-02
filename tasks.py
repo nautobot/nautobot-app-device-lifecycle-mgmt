nautobot/nautobot-app-device-onboarding

Cookie updated by NetworkToCode Cookie Drift Manager Tool
Template:

```
{
    "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
    "dir": "nautobot-app",
    "ref": "refs/tags/nautobot-app-v2.4.0",
    "path": null
}
```

Cookie:

```
{
    "remote": "https://github.com/nautobot/nautobot-app-device-onboarding.git",
    "path": "/tmp/tmp660tmw5x/nautobot-app-device-onboarding",
    "repository_path": "/tmp/tmp660tmw5x/nautobot-app-device-onboarding",
    "dir": "",
    "branch_prefix": "drift-manager",
    "context": {
        "codeowner_github_usernames": "@mzbroch @scetron @glennmatthews @chadell",
        "full_name": "Network to Code, LLC",
        "email": "info@networktocode.com",
        "github_org": "nautobot",
        "app_name": "nautobot_device_onboarding",
        "verbose_name": "Device Onboarding",
        "app_slug": "nautobot-device-onboarding",
        "project_slug": "nautobot-app-device-onboarding",
        "repo_url": "https://github.com/nautobot/nautobot-app-device-onboarding",
        "base_url": "nautobot-device-onboarding",
        "min_nautobot_version": "2.0.3",
        "max_nautobot_version": "2.9999",
        "camel_name": "NautobotDeviceOnboarding",
        "project_short_description": "Device Onboarding",
        "model_class_name": "None",
        "open_source_license": "Apache-2.0",
        "docs_base_url": "https://docs.nautobot.com",
        "docs_app_url": "https://docs.nautobot.com/projects/device-onboarding/en/latest",
        "_template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "_output_dir": "/tmp/tmp660tmw5x",
        "_repo_dir": "/github/home/.cookiecutters/cookiecutter-nautobot-app/nautobot-app",
        "_checkout": "refs/tags/nautobot-app-v2.4.0"
    },
    "base_branch": "develop",
    "remote_name": "origin",
    "pull_request_strategy": "PullRequestStrategy.CREATE",
    "post_actions": [
        "PostAction.RUFF",
        "PostAction.POETRY"
    ],
    "baked_commit

tasks.py
@@ -14,11 +14,12 @@ limitations under the License.
 
 import os
 import re
+import sys
 from pathlib import Path
 from time import sleep
 
 from invoke.collection import Collection
-from invoke.exceptions import Exit
+from invoke.exceptions import Exit, UnexpectedExit
 from invoke.tasks import task as invoke_task
 
 
@@ -156,7 +157,7 @@ def run_command(context, command, **kwargs):
                 **kwargs.get("env", {}),
                 **kwargs.pop("command_env"),
             }
-        context.run(command, **kwargs)
+        return context.run(command, **kwargs)
     else:
         # Check if nautobot is running, no need to start another nautobot container to run a command
         docker_compose_status = "ps --services --filter status=running"
@@ -175,7 +176,7 @@ def run_command(context, command, **kwargs):
 
         pty = kwargs.pop("pty", True)
 
-        docker_compose(context, compose_command, pty=pty, **kwargs)
+        return docker_compose(context, compose_command, pty=pty, **kwargs)
 
 
 # ------------------------------------------------------------------------------
@@ -249,9 +250,20 @@ def lock(context, check=False, constrain_nautobot_ver=False, constrain_python_ve
         command = f"poetry add --lock nautobot@{docker_nautobot_version}"
         if constrain_python_ver:
             command += f" --python {context.nautobot_device_onboarding.python_ver}"
+        try:
+            run_command(context, command, hide=True)
+            output = run_command(context, command, hide=True)
+            print(output.stdout, end="")
+            print(output.stderr, file=sys.stderr, end="")
+        except UnexpectedExit:
+            print("Unable to add Nautobot dependency with version constraint, falling back to git branch.")
+            command = f"poetry add --lock git+https://github.com/nautobot/nautobot.git#{context.nautobot_device_onboarding.nautobot_ver}"
+            if constrain_python_ver:
+                command += f" --python {context.nautobot_device_onboarding.python_ver}"
+            run_command(context, command)
     else:
         command = f"poetry {'check' if check else 'lock --no-update'}"
-    run_command(context, command)
+        run_command(context, command)
 
 
 # ------------------------------------------------------------------------------
@@ -681,10 +693,13 @@ def help_task(context):
 )
 def generate_release_notes(context, version=""):
     """Generate Release Notes using Towncrier."""
-    command = "env DJANGO_SETTINGS_MODULE=nautobot.core.settings towncrier build"
+    command = "poetry run towncrier build"
     if version:
         command += f" --version {version}"
-    run_command(context, command)
+    else:
+        command += " --version `poetry version -s`"
+    # Due to issues with git repo ownership in the containers, this must always run locally.
+    context.run(command)
 
 
 # ------------------------------------------------------------------------------
@@ -702,10 +717,27 @@ def hadolint(context):
 @task
 def pylint(context):
     """Run pylint code analysis."""
-    command = (
-        'pylint --init-hook "import nautobot; nautobot.setup()" --rcfile pyproject.toml nautobot_device_onboarding'
-    )
-    run_command(context, command)
+    exit_code = 0
+
+    base_pylint_command = 'pylint --verbose --init-hook "import nautobot; nautobot.setup()" --rcfile pyproject.toml'
+    command = f"{base_pylint_command} nautobot_device_onboarding"
+    if not run_command(context, command, warn=True):
+        exit_code = 1
+
+    # run the pylint_django migrations checkers on the migrations directory, if one exists
+    migrations_dir = Path(__file__).absolute().parent / Path("nautobot_device_onboarding") / Path("migrations")
+    if migrations_dir.is_dir():
+        migrations_pylint_command = (
+            f"{base_pylint_command} --load-plugins=pylint_django.checkers.migrations"
+            " --disable=all --enable=fatal,new-db-field-with-default,missing-backwards-migration-callable"
+            " nautobot_device_onboarding.migrations"
+        )
+        if not run_command(context, migrations_pylint_command, warn=True):
+            exit_code = 1
+    else:
+        print("No migrations directory found, skipping migrations checks.")
+
+    raise Exit(code=exit_code)
 
 
 @task(aliases=("a",))
@@ -730,12 +762,15 @@ def ruff(context, action=None, target=None, fix=False, output_format="concise"):
     if not target:
         target = ["."]
 
+    exit_code = 0
+
     if "format" in action:
         command = "ruff format "
         if not fix:
             command += "--check "
         command += " ".join(target)
-        run_command(context, command, warn=True)
+        if not run_command(context, command, warn=True):
+            exit_code = 1
 
     if "lint" in action:
         command = "ruff check "
@@ -743,7 +778,10 @@ def ruff(context, action=None, target=None, fix=False, output_format="concise"):
             command += "--fix "
         command += f"--output-format {output_format} "
         command += " ".join(target)
-        run_command(context, command, warn=True)
+        if not run_command(context, command, warn=True):
+            exit_code = 1
+
+    raise Exit(code=exit_code)
 
 
 @task

nautobot/nautobot-app-device-lifecycle-mgmt

    "remote": "https://github.com/nautobot/nautobot-app-device-lifecycle-mgmt.git",
    "path": "/tmp/tmp9s27urtv/nautobot-app-device-lifecycle-mgmt",
    "repository_path": "/tmp/tmp9s27urtv/nautobot-app-device-lifecycle-mgmt",
        "codeowner_github_usernames": "@josh5276 @jvanderaa @progala",
        "app_name": "nautobot_device_lifecycle_mgmt",
        "verbose_name": "Device Lifecycle Management",
        "app_slug": "nautobot-device-lifecycle-mgmt",
        "project_slug": "nautobot-app-device-lifecycle-mgmt",
        "repo_url": "https://github.com/nautobot/nautobot-app-device-lifecycle-mgmt",
        "base_url": "nautobot-device-lifecycle-mgmt",
        "min_nautobot_version": "2.0.0",
        "camel_name": "NautobotDeviceLifecycleManagement",
        "project_short_description": "Device Lifecycle Management",
        "docs_app_url": "https://docs.nautobot.com/projects/device-lifecycle/en/latest",
        "_output_dir": "/tmp/tmp9s27urtv",
    "baked_commit_ref": "3f6783c6371261fc6cffe4247c4ae238ce1284cb",

tasks.py
@@ -14,11 +14,12 @@ limitations under the License.
 
 import os
 import re
+import sys
 from pathlib import Path
 from time import sleep
 
 from invoke.collection import Collection
-from invoke.exceptions import Exit
+from invoke.exceptions import Exit, UnexpectedExit
 from invoke.tasks import task as invoke_task
 
 
@@ -156,7 +157,7 @@ def run_command(context, command, **kwargs):
                 **kwargs.get("env", {}),
                 **kwargs.pop("command_env"),
             }
-        context.run(command, **kwargs)
+        return context.run(command, **kwargs)
     else:
         # Check if nautobot is running, no need to start another nautobot container to run a command
         docker_compose_status = "ps --services --filter status=running"
@@ -175,7 +176,7 @@ def run_command(context, command, **kwargs):
 
         pty = kwargs.pop("pty", True)
 
-        docker_compose(context, compose_command, pty=pty, **kwargs)
+        return docker_compose(context, compose_command, pty=pty, **kwargs)
 
 
 # ------------------------------------------------------------------------------
@@ -249,9 +250,20 @@ def lock(context, check=False, constrain_nautobot_ver=False, constrain_python_ve
         command = f"poetry add --lock nautobot@{docker_nautobot_version}"
         if constrain_python_ver:
             command += f" --python {context.nautobot_device_lifecycle_mgmt.python_ver}"
+        try:
+            run_command(context, command, hide=True)
+            output = run_command(context, command, hide=True)
+            print(output.stdout, end="")
+            print(output.stderr, file=sys.stderr, end="")
+        except UnexpectedExit:
+            print("Unable to add Nautobot dependency with version constraint, falling back to git branch.")
+            command = f"poetry add --lock git+https://github.com/nautobot/nautobot.git#{context.nautobot_device_lifecycle_mgmt.nautobot_ver}"
+            if constrain_python_ver:
+                command += f" --python {context.nautobot_device_lifecycle_mgmt.python_ver}"
+            run_command(context, command)
     else:
         command = f"poetry {'check' if check else 'lock --no-update'}"
-    run_command(context, command)
+        run_command(context, command)
 
 
 # ------------------------------------------------------------------------------
@@ -681,10 +693,13 @@ def help_task(context):
 )
 def generate_release_notes(context, version=""):
     """Generate Release Notes using Towncrier."""
-    command = "env DJANGO_SETTINGS_MODULE=nautobot.core.settings towncrier build"
+    command = "poetry run towncrier build"
     if version:
         command += f" --version {version}"
-    run_command(context, command)
+    else:
+        command += " --version `poetry version -s`"
+    # Due to issues with git repo ownership in the containers, this must always run locally.
+    context.run(command)
 
 
 # ------------------------------------------------------------------------------
@@ -702,10 +717,27 @@ def hadolint(context):
 @task
 def pylint(context):
     """Run pylint code analysis."""
-    command = (
-        'pylint --init-hook "import nautobot; nautobot.setup()" --rcfile pyproject.toml nautobot_device_lifecycle_mgmt'
-    )
-    run_command(context, command)
+    exit_code = 0
+
+    base_pylint_command = 'pylint --verbose --init-hook "import nautobot; nautobot.setup()" --rcfile pyproject.toml'
+    command = f"{base_pylint_command} nautobot_device_lifecycle_mgmt"
+    if not run_command(context, command, warn=True):
+        exit_code = 1
+
+    # run the pylint_django migrations checkers on the migrations directory, if one exists
+    migrations_dir = Path(__file__).absolute().parent / Path("nautobot_device_lifecycle_mgmt") / Path("migrations")
+    if migrations_dir.is_dir():
+        migrations_pylint_command = (
+            f"{base_pylint_command} --load-plugins=pylint_django.checkers.migrations"
+            " --disable=all --enable=fatal,new-db-field-with-default,missing-backwards-migration-callable"
+            " nautobot_device_lifecycle_mgmt.migrations"
+        )
+        if not run_command(context, migrations_pylint_command, warn=True):
+            exit_code = 1
+    else:
+        print("No migrations directory found, skipping migrations checks.")
+
+    raise Exit(code=exit_code)
 
 
 @task(aliases=("a",))
@@ -730,12 +762,15 @@ def ruff(context, action=None, target=None, fix=False, output_format="concise"):
     if not target:
         target = ["."]
 
+    exit_code = 0
+
     if "format" in action:
         command = "ruff format "
         if not fix:
             command += "--check "
         command += " ".join(target)
-        run_command(context, command, warn=True)
+        if not run_command(context, command, warn=True):
+            exit_code = 1
 
     if "lint" in action:
         command = "ruff check "
@@ -743,7 +778,10 @@ def ruff(context, action=None, target=None, fix=False, output_format="concise"):
             command += "--fix "
         command += f"--output-format {output_format} "
         command += " ".join(target)
-        run_command(context, command, warn=True)
+        if not run_command(context, command, warn=True):
+            exit_code = 1
+
+    raise Exit(code=exit_code)
 
 
 @task