8e3e7431bd8cef73f77c49190a2053b693a68ba5
Przemek Rogala <progala@progala.net>
2025-10-17 10:44:15 +0100

    Fix the VulnerabilityLCMViewTest test

--- nautobot_device_lifecycle_mgmt/tests/test_views.py
+++ nautobot_device_lifecycle_mgmt/tests/test_views.py
@@ -266,6 +266,7 @@ class VulnerabilityLCMViewTest(ViewTestCases.PrimaryObjectViewTestCase):
     """Test the VulnerabilityLCM views."""
 
     model = VulnerabilityLCM
+    allowed_number_of_tree_queries_per_view_type = {"retrieve": 1}
 
     @classmethod
     def setUpTestData(cls):  # pylint: disable=invalid-name
